716b12d51e3d96f1f116dce3eef296bc36496246
Marko Novak <marko.novak82@gmail.com>
2013-05-09 10:56:43 +0200

    Images for obstacles Improved hit detection

--- index.php
+++ index.php
@@ -2,18 +2,20 @@
 <head>
 	<title>Speed Racer</title>
 	<meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-	<script src="js/global.js?v=<?= rand(0, 1000000000000000) ?>"></script>
 	<link rel="stylesheet" href="css/global.css">
 </head>
 <body>
-	<div class="container">
-		<div class="speed"></div>
-		<div class="road"></div>
-		<div class="grass left"></div>
-		<div class="grass right"></div>
-		<div class="car"></div>
+	<div class="center">
+		<div class="container">
+			<div class="speed"></div>
+			<div class="road"></div>
+			<div class="grass left"></div>
+			<div class="grass right"></div>
+			<div class="car"></div>
+		</div>
 	</div>
-	<!--<img src="images/wheel.jpg" alt="wheel" width="280" height="280">-->
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="js/global.js?v=<?= rand(0, 1000000000000000) ?>"></script>
 </body>
 </html>
--- js/global.js
+++ js/global.js
@@ -10,22 +10,22 @@ $(document).ready(function () {
 function Game (options) {
 	this.baseURL            = 'http://www.killerbee.si/speedracer/';
     this.wheelPosition      = 0;
-    this.speed              = 200;
-    this.speedArray         = [];
+    this.speed              = 0;
+    this._maxSpeed          = 200;
+    this._grassSpeed        = 120;
     this.wheelArray         = [];
     this.obstaclesArray     = [];
     this._slidingWindow     = 3;
-    this._speedMax          = 200;
-    this._speedMin          = 50;
     this._carHandlingFactor = 2000;
-    this._carSteeringFactor = 120;
     this._speedFactor       = 5;
     this._gameWidth         = $(document).width();
     this._gameHeight        = $(document).height();
+    this.gameRunning        = false;
     this._grassWidth        = $('.grass').width();
     this._numLanes          = 12;
     this._gameContainer     = $('.container');
     this.stopObstacles      = false;
+    this.obstaclesTriggered = false;
 
     this._road = {
         node: $('.road'),
@@ -53,16 +53,23 @@ function Game (options) {
     };
     this._car = {
         node: $('.car'),
-        position: { 
-            x: Math.round(this._gameWidth / 2), 
-            y: parseInt($('.car').css('bottom'), 10) 
-        },
         size: { 
             width: parseInt($('.car').css('width'), 10),
             height: parseInt($('.car').css('height'), 10)
         },
+        position: { 
+            x: Math.round(this._gameWidth / 2), 
+            y: 300
+        },
         rotation: 0
     };
+    this._carHitpoint = {
+        node: $('.car-hitpoint'),
+        position: {
+            x: Math.round(this._gameWidth / 2), 
+            y: 300
+        }
+    }
     this._speedometer = {
         node: $('.speed')
     };
@@ -73,43 +80,33 @@ function Game (options) {
 Game.prototype.init = function () {
 	// getting orientation data from window
 	window.addEventListener('deviceorientation', this.deviceOrientation.bind(this));
+    
+    // set car to the right position
+    this._car.node.css({ '-webkit-transform': 'translate3D(' + this._car.position.x + 'px, 0, 0) rotateZ(' + this._car.rotation + 'deg)' });
 
-	this.start();
+    this.start();
 };
 
 Game.prototype.deviceOrientation = function (e) {
-    var currentSpeed,
-        speedTotal = 0,
-        wheelTotal = 0;
-
-    // if (e.gamma < -45 && e.gamma > -90) {
-    //     this.speedArray.push(Math.round((90 + e.gamma) * this._speedFactor));
-        
-    //     if (this.speedArray.length > this._slidingWindow) this.speedArray.splice(0,1);
-        
-    //     for (var i = 0; i < this.speedArray.length; i++) {
-    //         speedTotal += this.speedArray[i];
-    //     }
-
-    //     this.speed = Math.min(200, Math.max(50, Math.round(speedTotal / this.speedArray.length)));
-    // }
+    var wheelTotal = 0;
 
     if (e.beta < 90 && e.beta > -90)
         this.wheelArray.push(e.beta);
 
-        if (this.wheelArray.length > this._slidingWindow) this.wheelArray.splice(0,1);
+    if (this.wheelArray.length > this._slidingWindow) 
+        this.wheelArray.splice(0,1);
 
-        for (var i = 0; i < this.wheelArray.length; i++) {
-            wheelTotal += this.wheelArray[i];
-        }
+    for (var i = 0; i < this.wheelArray.length; i++) {
+        wheelTotal += this.wheelArray[i];
+    }
 
-        this.wheelPosition = Math.round(wheelTotal / this.wheelArray.length);
+    this.wheelPosition = Math.round(wheelTotal / this.wheelArray.length);
 };
 
 Game.prototype.start = function () {
     this.startTime = +new Date;
 
-    this.triggerObstacles();
+    this.gameRunning = true;
 
 	this.tick();
 };
@@ -120,7 +117,8 @@ Game.prototype.tick = function () {
         currentTime,
         countdownTime;
 
-    this._rafId = requestAnimationFrame(this.tick);
+    if (this.gameRunning)
+        this._rafId = requestAnimationFrame(this.tick);
 
     currentTime = +new Date;
     if (this.lastUpdateTime == null)
@@ -139,14 +137,27 @@ Game.prototype.tick = function () {
 };
 
 Game.prototype.update = function (elapsed, countdownTime) {
+    if (this.speed < this._maxSpeed && !this.carTwitching) {
+        this.speed += 2;
+    } else {
+        if (!this.obstaclesTriggered) {
+            this.triggerObstacles();
+            this.obstaclesTriggered = true;
+        } 
+    }
+        
+
     // car on grass
     if (this._car.position.x < this._grassWidth - Math.round(this._car.size.width / 2) || this._car.position.x > this._gameWidth - this._grassWidth - Math.round(this._car.size.width / 2)) {
-        this.currentSpeed = Math.round(this.speed / 1.5);
+        if (this.speed > this._grassSpeed)
+            this.speed -= 2;
         this.carTwitching = true;
     } else {
-        this.currentSpeed = this.speed;
         this.carTwitching = false;
     }
+    this.currentSpeed = this.speed;
+
+    var basicYPosition = Math.round((this.currentSpeed * elapsed) / 1000);
 
     // update obstacles
     if (this.obstaclesArray.length > 1) {
@@ -154,7 +165,7 @@ Game.prototype.update = function (elapsed, countdownTime) {
             // if moving obstacle detects other obstacle in front of it, match speed
             if (this.obstaclesArray[i].motion == 'moving') {
                 for (var j = 0; j < this.obstaclesArray.length; j++) {
-                    if (this.obstaclesArray[j].lane == this.obstaclesArray[i].lane && (this.obstaclesArray[j].position.y + 20) > this.obstaclesArray[i].position.y) {
+                    if (this.obstaclesArray[j].lane == this.obstaclesArray[i].lane && (this.obstaclesArray[j].position.y + 40) > this.obstaclesArray[i].position.y) {
                         this.obstaclesArray[i].speed = this.obstaclesArray[j].speed;
                     }
                 }
@@ -170,16 +181,16 @@ Game.prototype.update = function (elapsed, countdownTime) {
     }
 
     // update road position
-    this._road.position.y += Math.round((this.currentSpeed * elapsed) / 1000);
+    this._road.position.y += basicYPosition;
     if (this._road.position.y >= 0)
         this._road.position.y = -this._road.size.height;
 
     // update grass position
-    this._grassLeft.position.y += Math.round((this.currentSpeed * elapsed) / 1000);
+    this._grassLeft.position.y += basicYPosition;
     if (this._grassLeft.position.y >= 0)
         this._grassLeft.position.y = -this._grassLeft.size.height;
 
-    this._grassRight.position.y += Math.round((this.currentSpeed * elapsed) / 1000);
+    this._grassRight.position.y += basicYPosition;
     if (this._grassRight.position.y >= 0)
         this._grassRight.position.y = -this._grassRight.size.height;
 
@@ -195,6 +206,8 @@ Game.prototype.update = function (elapsed, countdownTime) {
         this._car.position.x = this._road.size.width - this._car.size.width;
         this._car.rotation = 0;
     }
+
+    this.checkCollisions();
 };
 
 Game.prototype.draw = function () {
@@ -203,7 +216,7 @@ Game.prototype.draw = function () {
     this._grassLeft.node.css({ '-webkit-transform': 'translate3D(0, ' + this._grassLeft.position.y + 'px, 0)' });
     this._grassRight.node.css({ '-webkit-transform': 'translate3D(0, ' + this._grassRight.position.y + 'px, 0)' });
     this._car.node.css({ '-webkit-transform': 'translate3D(' + this._car.position.x + 'px, 0, 0) rotateZ(' + this._car.rotation + 'deg)' });
-    
+
     if (this.obstaclesArray.length > 0) {
         for (var i = 0; i < this.obstaclesArray.length; i++) {
             this.obstaclesArray[i].node.css({ '-webkit-transform': 'translate3D(' + this.obstaclesArray[i].position.x + 'px, ' + this.obstaclesArray[i].position.y + 'px, 0)' });
@@ -213,7 +226,7 @@ Game.prototype.draw = function () {
 
 Game.prototype.triggerObstacles = function () {
     var self = this,
-        randomTimeOffset = getRandom(100,400),
+        randomTimeOffset = getRandom(200,500),
         randomLane;
 
     setTimeout(function () {
@@ -224,6 +237,7 @@ Game.prototype.triggerObstacles = function () {
 
 Game.prototype.createObstacle = function () {
     var obstacle,
+        obstacleNode,
         type = getRandom(1,6),
         lane = getRandom(1,this._numLanes),
         speed;
@@ -254,17 +268,27 @@ Game.prototype.createObstacle = function () {
             motion = 'moving';
     }
 
+    obstacleNode = $('<div></div>').addClass('obstacle').addClass('type-' + type);
+
     obstacle = {
-        node: $('<div></div>').addClass('obstacle').addClass('type-' + type),
+        node: obstacleNode,
         type: type,
         lane: lane,
         motion: motion,
-        position: { x: (((lane - 1) * Math.round((this._gameWidth - (2 * this._grassWidth)) / this._numLanes))) + this._grassWidth, y: -50 },
+        position: { 
+            x: (((lane - 1) * Math.round((this._gameWidth - (2 * this._grassWidth)) / this._numLanes))) + this._grassWidth + 2, 
+            y: -50 
+        },
         speed: speed
     };
 
     this.obstaclesArray.push(obstacle);
     this._gameContainer.append(obstacle.node);
+
+    obstacle.size = {
+        width: parseInt(obstacleNode.width(), 10),
+        height: parseInt(obstacleNode.width(), 10)
+    };
 };
 
 Game.prototype.destroyObstacle = function (obstacleId) {
@@ -273,13 +297,7 @@ Game.prototype.destroyObstacle = function (obstacleId) {
 };
 
 Game.prototype.getCarRotation = function () {
-    var carRotation;
-
-    if (this.carTwitching)
-        carRotation = this.wheelPosition + getRandom(-5,5);
-    else
-        carRotation = this.wheelPosition;
-
+    var carRotation = (this.carTwitching) ? this.wheelPosition + getRandom(-5,5) : this.wheelPosition;
     return Math.min(70, carRotation);
 };
 
@@ -287,9 +305,35 @@ Game.prototype.getCarPosition = function () {
     return this.wheelPosition * (this.currentSpeed / this._carHandlingFactor);
 };
 
-Game.prototype.checkCollision = function (obstacle) {
-    // check collisions
-    // you will set this.stopObstacles on hit and set speed to 0
+Game.prototype.checkCollisions = function () {
+    for (var i = 0; i < this.obstaclesArray.length; i++) {
+        var obstacleHitpoint = {
+            x: Math.round(this.obstaclesArray[i].position.x + (this.obstaclesArray[i].size.width / 2)), // middle of the obstacle
+            y: this.obstaclesArray[i].position.y + this.obstaclesArray[i].size.height // back of the obstacle
+        };
+        var carHitpoint = {
+            x: Math.round(this._car.position.x + (this._car.size.width / 2)), // middle of the car
+            y: this._car.position.y // front of the car
+        };
+        if (Math.abs(obstacleHitpoint.x - carHitpoint.x) < this.obstaclesArray[i].size.width && obstacleHitpoint.y - carHitpoint.y > 0 && obstacleHitpoint.y - carHitpoint.y < this.obstaclesArray[i].size.height) {
+            if (!this.collisionDetected)
+                this.triggerCollision(this.obstaclesArray[i].type);
+        }
+    }
+};
+
+Game.prototype.triggerCollision = function (obstacleType) {
+    alert('CRASH!!!!!, obstacle type: ' + obstacleType);
+    this.collisionDetected = true;
+    this.stopGame();
+};
+
+Game.prototype.stopGame = function () {
+    window.removeEventListener('deviceorientation', this.deviceOrientation);
+    this.speed = 0;
+    this.currentSpeed = 0;
+    this.stopObstacles = true;
+    this.gameRunning = false;
 };
 
 Game.prototype.setCarPosition = function () {
